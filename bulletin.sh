ab1d9a604c1f38a73586ff180821b5b8335f8cdd
Andrew Nisbet <anisbet@epl.ca>
2012-12-05 15:19:34 -0700

    Fixed bulletin.sh to clean up after itself and work with make.

--- bulletin.sh
+++ bulletin.sh
@@ -17,19 +17,25 @@
 #
 # Collects all the notices required for the reports in the reports/ directory
 #
-REPORT_DIR=reports
-BULLETIN_DIR=bulletins
+
 SERVER=eplapp.library.ualberta.ca
 USER=sirsi
-REMOTE=/s/sirsi/Unicorn/Notices/
-LIST_OF_BULLETINS=$BULLETIN_DIR/bulletins.lst
+REMOTE="/s/sirsi/Unicorn/Notices"
+LIST_OF_BULLETINS="bulletins.lst"
+LIST_OF_BULLETIN_FILES="notice.lst"
+REPORT_DIR="reports"
+BULLETIN_DIR="bulletins"
+
 # Get a list every time a report makes reference to a .read tag:
 grep "\.read " $REPORT_DIR/*.prn | cut -d":" -f2 >$LIST_OF_BULLETINS
 # remove the spaces and get back the last field
-sed -e 's/\// /g' notices.txt | awk '{print $NF}' | sort | uniq -c | cut -c9- >$LIST_OF_BULLETINS
+sed -e 's/\// /g' $LIST_OF_BULLETINS | awk '{print $NF}' | sort | uniq -c | cut -c9- >$LIST_OF_BULLETIN_FILES
 
-for i in $(cat $LIST_OF_BULLETINS)
+for i in $(cat $LIST_OF_BULLETIN_FILES)
 do
 	# printf "%s\n" "$i"
-	scp $USER\@$SERVER:$REMOTE$i $BULLETIN_DIR
+	scp $USER\@$SERVER:$REMOTE/$i $BULLETIN_DIR
 done
+
+rm ${LIST_OF_BULLETIN_FILES}
+rm ${LIST_OF_BULLETINS}
