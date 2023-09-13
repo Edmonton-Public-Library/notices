ec9be6f91ddd420d4c1c28d66b1a84d7eeeec7f8
anisbet <andrew@dev-ils.com>
2023-09-12 23:21:08 -0400

    Improved readability of __str__() method.

--- customer.py
+++ customer.py
@@ -59,6 +59,9 @@ class ItemBlock:
         
     def __str__( self ):
         return ' '.join( self.itemLines )
+
+    def __repr__(self):
+        return [x.strip() for x in self.itemLines]
         
 # This class represents a customer who is potentially going to receive a notice from 
 # the library. A Customer knows if its address is well formatted and can answer the 
@@ -290,11 +293,7 @@ class Customer:
             return []
             
     def __str__( self ):
-        output =  '[CUSTOMER\ncustomer receives mail notices: '+str(self.getsPrintedNotices())+'\n'
-        output += 'customer\'s mail address is valid: '+str(self.isWellFormed())+'\n'
-        output += 'items for this customer include: %d\n' % len( self.items )
-        output += 'address block: \n' + str(self.addressBlock)
-        return output + 'CUSTOMER]\n'
+        return f"address: {self.addressBlock.__repr__()}, snail: {self.getsPrintedNotices()}, wellformed: {self.isWellFormed()}, itemcount: {len( self.items )}"
     
     # Returns true if the customer's mail address is complete and valid
     # and False otherwise. The last line of an address must be a postal code.
